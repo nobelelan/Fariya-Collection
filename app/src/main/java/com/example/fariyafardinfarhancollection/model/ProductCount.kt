cc096697641a2184dc4838f3d8375d666ab25ce0
Nobel Elan <nobelelan@gmail.com>
2023-01-06 18:36:43 +0600

    userapp, implemented some design

---
+++ app/src/main/java/com/example/fariyafardinfarhancollection/model/ProductCount.kt
@@ -0,0 +1,9 @@
+package com.example.fariyafardinfarhancollection.model
+
+data class ProductCount(
+    val id: Int = 1,
+    val name: String = "",
+    val quantity: Int = 0,
+    val price: Int = 0,
+    val total: Int = 0
+)
--- app/src/main/java/com/example/fariyafardinfarhancollection/ui/fragment/SaleFragment.kt
+++ app/src/main/java/com/example/fariyafardinfarhancollection/ui/fragment/SaleFragment.kt
@@ -6,9 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.fariyafardinfarhancollection.R
+import com.example.fariyafardinfarhancollection.databinding.FragmentSaleBinding
 
 
 class SaleFragment : Fragment() {
+
+    private var _binding: FragmentSaleBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,4 +23,14 @@ class SaleFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_sale, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentSaleBinding.bind(view)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
